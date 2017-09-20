f721489c718090834c58d26dfa44af6369555f15
zhangshubo <2294858483@qq.com>
2017-09-20 16:58:13 +0800

    完成启动页面

---
+++ app/src/main/java/com/example/bsz/words/Welcome.java
@@ -0,0 +1,35 @@
+package com.example.bsz.words;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class Welcome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);  //隐藏标题栏和状态栏
+        requestWindowFeature(Window.FEATURE_NO_TITLE);//去掉标题
+        setContentView(R.layout.activity_welcome);
+        handler.sendEmptyMessageDelayed(0,3000);
+
+    }
+    private Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            getHome();
+            super.handleMessage(msg);
+        }
+    };
+    public void getHome(){
+        Intent intent = new Intent(Welcome.this,MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
